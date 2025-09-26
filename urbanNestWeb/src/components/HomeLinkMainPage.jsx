4b65ca8c7d4c1bbcb015148fc59f728c6a6aa5dc
Igor89-hub <igorviyusan@gmail.com>
2025-09-26 12:27:42 +0200

    Logoa Jarrita

--- urbanNestWeb/src/components/HomeLinkMainPage.jsx
+++ urbanNestWeb/src/components/HomeLinkMainPage.jsx
@@ -1,9 +1,25 @@
+import { useState } from "react"
+
 export function HomeLinkMainPage() {
+
+    const [isDivClicked, setDivClicked] = useState('alquiler') //Defektuz egongo da;
     return (
         <div className="hl-mainLayout">
             <header className="hl-mainHeaderTitle">
-                <h2 className="hl-mainTitle">HOLAAAA</h2>
+                <img className="hl-mainHeader-logo" alt="HomeLink Logo" src="/images/homeLinkLogo.png" />
+                <span className="hl-mainHeader-welcomeText">!La busqueda de lo nuevo comieza aqui!</span>
             </header>
+            <div className="hl-mainLayout-carrusel">
+                <div className="hl-mainLayout-selector">
+                    <div className={`hl-mainLayout-selectorLeft ${isDivClicked == 'alquiler' ? 'active' : ''}`} onClick={() => setDivClicked('alquiler')}>
+                        <p className="hl-mainLayout-selectorAlquiler">Alquiler</p>
+                    </div>
+                    <div className={`hl-mainLayout-selectorRight ${isDivClicked == 'compra' ? 'active' : ''}`} onClick={() => setDivClicked('compra')}>
+                        <p className="hl-mainLayout-selectorcompra">Compra</p>
+                    </div>
+                </div>
+                <div className="h1-mainLayout-autoComplete"></div>
+            </div>
         </div>
     )
 }
