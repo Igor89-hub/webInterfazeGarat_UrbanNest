4401edec4943a2a5185e3ebb02e60ed82a827984
Igor89-hub <igorviyusan@gmail.com>
2025-09-29 12:43:56 +0200

    textu animazioak ongi jarrita

--- urbanNestWeb/src/components/HomeLinkMainPage.jsx
+++ urbanNestWeb/src/components/HomeLinkMainPage.jsx
@@ -1,8 +1,31 @@
 import { useEffect, useState } from "react";
 import { ImageCarrusel } from "./ImageCarrusel";
+import { useRef } from "react";
+import { gsap } from "gsap";
+import { SplitText } from "gsap/SplitText";
 import "../css/Carrusel.css";
+import { TimelineMax } from "gsap/gsap-core";
 
 export function HomeLinkMainPage() {
+
+    useEffect(() => {
+        const split = new SplitText(".hl-mainLayout-title", {
+            type:"lines,words"
+        });
+
+        gsap.from(split.words, {
+            duration: 1,
+            y: 40,
+            opacity: 0,
+            stagger: 0.1,
+            ease: "power3.out"
+        });
+
+        return () => {
+            split.revert();
+        };
+
+    }, []);
 
     const [isDivClicked, setDivClicked] = useState('alquiler') //Defektuz egongo da;
 
@@ -13,7 +36,10 @@ export function HomeLinkMainPage() {
                 <span className="hl-mainHeader-welcomeText">¡La busqueda de lo nuevo comieza aqui!</span>
             </header>
             <div className="hl-mainLayout-carrusel">
-                <ImageCarrusel/>
+                <ImageCarrusel />
+                <div className="hl-mainLayout-dividerTitle">
+                    <p className="hl-mainLayout-title">Gran mercado inmobiliario de HomeLink</p>
+                </div>
                 <div className="hl-mainLayout-selector">
                     <div className={`hl-mainLayout-selectorLeft ${isDivClicked == 'alquiler' ? 'active' : ''}`} onClick={() => setDivClicked('alquiler')}>
                         <p className="hl-mainLayout-selectorAlquiler">Alquiler</p>
